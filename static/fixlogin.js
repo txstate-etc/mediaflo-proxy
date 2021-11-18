36395b145b4c86281beb25661ce59dc2927d523f
Nick Wing <nwing@txstate.edu>
2021-11-18 14:18:48 -0600

    last commit added a fix for login, this one catches the case where the user hits return to submit before onchange

--- static/fixlogin.js
+++ static/fixlogin.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.getElementById('ctl00_pageContent_LoginControl_userNameInput') || document.getElementById('ctl00_MainContent_login_userNameInput')
   if (!input) return
-  input.addEventListener('change', function (e) {
+  const fix = () => {
     const val = input.value
     const cleanval = val.trim().toLocaleLowerCase()
     if (val !== cleanval) input.value = cleanval
+  }
+  input.addEventListener('change', fix)
+  input.addEventListener('keydown', e => {
+    if (e.key === 'Enter') fix()
   })
 })
