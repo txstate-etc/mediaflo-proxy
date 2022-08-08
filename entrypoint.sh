0276d479dbb3f1edbde88f86945446ca7aa83bad
Jeff Snider <jsnider@txstate.edu>
2022-08-08 12:37:11 -0500

    Add redirect for /services/oembed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${ENSEMBLE_HOST} ${MFREV_HOST}' < /etc/nginx/locations.conf.template > /etc/nginx/locations.conf
+envsubst '${ENSEMBLE_HOST} ${MFREV_HOST} ${SERVICES_OEMBED_REDIR_HOST}' < /etc/nginx/locations.conf.template > /etc/nginx/locations.conf
 
 exec "$@"
